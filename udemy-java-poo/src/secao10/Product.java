c6d1b1abba24dfa65e818fc7602aa43acd625502
onatao <nathanbarrosnt@gmail.com>
2023-02-14 16:40:27 -0300

    ..

---
+++ udemy-java-poo/src/secao10/Product.java
@@ -0,0 +1,32 @@
+package secao10;
+
+import java.util.jar.Attributes.Name;
+
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+     
+    
+}
